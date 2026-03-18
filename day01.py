b35db3f9f09c763dec518e3cd174f2fbefb40d53
Ratan Bambule <ratanbambule.123@gmail.com>
2026-03-18 23:53:57 +0530

    Day 1: added first print statements

---
+++ day01.py
@@ -0,0 +1,11 @@
+print("Hello, I am Ratan")
+print("I am senior QA engineer at Nurix.ai")
+print("Python will help me become a LLM Evaluation Engineer")
+print("My targer: 25 to 35 LPA remote")
+
+#My Home work
+print("--------------------")
+print("Ratan Ravishankr Bambule")
+print("Nurix.ai")
+print("I want to automate platform testing using claude as a tool")
+print("I want to automate whole NuPlay platform")
